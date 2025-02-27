07ea421c1baf2e97af168c774f81c64abf4f5d8e
Faza Murtadho <fazamurtadho@softwareseni.com>
2025-02-27 15:57:08 +0700

    feat: static data

---
+++ src/data/hiraganaData.tsx
@@ -0,0 +1,23 @@
+export const hiraganaListVowels = [
+  { id: 1, hiragana: "あ", romaji: "a" },
+  { id: 2, hiragana: "い", romaji: "i" },
+  { id: 3, hiragana: "う", romaji: "u" },
+  { id: 4, hiragana: "え", romaji: "e" },
+  { id: 5, hiragana: "お", romaji: "o" },
+];
+
+export const hiraganaListKagyou = [
+  { id: 6, hiragana: "か", romaji: "ka" },
+  { id: 7, hiragana: "き", romaji: "ki" },
+  { id: 8, hiragana: "く", romaji: "ku" },
+  { id: 9, hiragana: "け", romaji: "ke" },
+  { id: 10, hiragana: "こ", romaji: "ko" },
+];
+
+export const hiraganaListSagyou = [
+  { id: 11, hiragana: "さ", romaji: "sa" },
+  { id: 12, hiragana: "し", romaji: "shi" },
+  { id: 13, hiragana: "す", romaji: "su" },
+  { id: 14, hiragana: "せ", romaji: "se" },
+  { id: 15, hiragana: "そ", romaji: "so" },
+];
---
+++ src/data/katakanaData.tsx
@@ -0,0 +1,23 @@
+export const katakanaListVowels = [
+  { id: 1, katakana: "ア", romaji: "a" },
+  { id: 2, katakana: "イ", romaji: "i" },
+  { id: 3, katakana: "ウ", romaji: "u" },
+  { id: 4, katakana: "エ", romaji: "e" },
+  { id: 5, katakana: "オ", romaji: "o" },
+];
+
+export const katakanaListKagyou = [
+  { id: 6, katakana: "カ", romaji: "ka" },
+  { id: 7, katakana: "キ", romaji: "ki" },
+  { id: 8, katakana: "ク", romaji: "ku" },
+  { id: 9, katakana: "ケ", romaji: "ke" },
+  { id: 10, katakana: "コ", romaji: "ko" },
+];
+
+export const katakanaListSagyou = [
+  { id: 11, katakana: "サ", romaji: "sa" },
+  { id: 12, katakana: "シ", romaji: "shi" },
+  { id: 13, katakana: "ス", romaji: "su" },
+  { id: 14, katakana: "セ", romaji: "se" },
+  { id: 15, katakana: "ソ", romaji: "so" },
+];
