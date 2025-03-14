3d08850233d580028fef48db362c1c3ba87bd377
Faza Murtadho <fazamurtadho@softwareseni.com>
2025-03-14 08:09:19 +0700

    feat: renaming and adding static data

---
+++ src/data/hiragana-combi-data.tsx
@@ -0,0 +1,23 @@
+export const hiraganaCombiListKyagyou = [
+  { id: 1, hiraganaCombi: "きゃ", romaji: "kya" },
+  { id: 2, hiraganaCombi: "きゅ", romaji: "kyu" },
+  { id: 3, hiraganaCombi: "きょ", romaji: "kyo" },
+];
+
+export const hiraganaCombiListShagyou = [
+  { id: 4, hiraganaCombi: "しゃ", romaji: "sha" },
+  { id: 5, hiraganaCombi: "しゅ", romaji: "shu" },
+  { id: 6, hiraganaCombi: "しょ", romaji: "sho" },
+];
+
+export const hiraganaCombiListChagyou = [
+  { id: 7, hiraganaCombi: "ちゃ", romaji: "cha" },
+  { id: 8, hiraganaCombi: "ちゅ", romaji: "chu" },
+  { id: 9, hiraganaCombi: "ちょ", romaji: "cho" },
+];
+
+export const hiraganaCombiListNyagyou = [
+  { id: 10, hiraganaCombi: "ちゃ", romaji: "cha" },
+  { id: 11, hiraganaCombi: "ちゅ", romaji: "chu" },
+  { id: 12, hiraganaCombi: "ちょ", romaji: "cho" },
+];
